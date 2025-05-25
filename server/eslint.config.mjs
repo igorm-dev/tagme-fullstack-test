51f2dea45c4f96e7e133881782a1aab95548fab6
igorm_dev <igoribeirom@gmail.com>
2025-05-25 18:14:58 -0300

    chore: Eslint, Prettier, VSCode configs

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -1,8 +1,10 @@
+// eslint.config.ts ou .mjs
+
 // @ts-check
 import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default tseslint.config(
   {
@@ -10,14 +12,13 @@ export default tseslint.config(
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
   {
     languageOptions: {
       globals: {
         ...globals.node,
         ...globals.jest,
       },
-      ecmaVersion: 5,
+      ecmaVersion: 'latest', // antes estava "5"
       sourceType: 'module',
       parserOptions: {
         projectService: true,
@@ -29,7 +30,9 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
     },
   },
+  // ⚠️ Sempre por último
+  eslintPluginPrettierRecommended,
 );
